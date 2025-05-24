590df6b8aa228e35629c5bbd734e3220dd60cbcc
sumup kristiyan-kiryakov <kristiyan.kiryakov@sumup.com>
2025-05-24 13:39:06 +0300

    implement expression and return statement parsing, add String methods for AST nodes

--- monkey/lexer/lexer_test.go
+++ monkey/lexer/lexer_test.go
@@ -1,7 +1,6 @@
 package lexer
 
 import (
-	"fmt"
 	"monkey/token"
 	"testing"
 )
@@ -108,7 +107,7 @@ return false;
 
 	for i, tt := range tests {
 		tok := l.NextToken()
-		fmt.Printf("tokentype=%q, literal=%q\n", tok.Type, tok.Literal)
+
 		if tok.Type != tt.expectedType {
 			t.Fatalf("tests[%d] - tokentype.wrong. expected=%q, got=%q",
 				i, tt.expectedType, tok.Type)
