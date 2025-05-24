ef964edddccb0cee94c27ffbc73a973b49aa9bc4
sumup kristiyan-kiryakov <kristiyan.kiryakov@sumup.com>
2025-05-24 19:25:25 +0300

    implement prefix expression and integer literal parsing in the AST and parser

--- monkey/ast/ast.go
+++ monkey/ast/ast.go
@@ -20,6 +20,48 @@ type Expression interface {
 	expressionNode()
 }
 
+type PrefixExpression struct {
+	Token    token.Token // the prefix token, e.g., '!'
+	Operator string
+	Right    Expression
+}
+
+func (pe *PrefixExpression) String() string {
+	var out bytes.Buffer
+
+	out.WriteString("(")
+	out.WriteString(pe.Operator)
+	out.WriteString(pe.Right.String())
+	out.WriteString(")")
+
+	return out.String()
+}
+
+func (pe *PrefixExpression) TokenLiteral() string {
+	return pe.Token.Literal
+}
+
+func (pe *PrefixExpression) expressionNode() {
+
+}
+
+type IntegerLiteral struct {
+	Token token.Token
+	Value int64
+}
+
+func (il *IntegerLiteral) String() string {
+	return il.Token.Literal
+}
+
+func (il *IntegerLiteral) expressionNode() {
+
+}
+
+func (il *IntegerLiteral) TokenLiteral() string {
+	return il.Token.Literal
+}
+
 type ExpressionStatement struct {
 	Token      token.Token // the first token of the expression
 	Expression Expression
