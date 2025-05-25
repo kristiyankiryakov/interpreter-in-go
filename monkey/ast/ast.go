fe8bb8a3b6ed56eb465b49997cca611a21f5f62d
sumup kristiyan-kiryakov <kristiyan.kiryakov@sumup.com>
2025-05-25 20:18:36 +0300

    add boolean and infix expression parsing to AST and parser

--- monkey/ast/ast.go
+++ monkey/ast/ast.go
@@ -20,6 +20,50 @@ type Expression interface {
 	expressionNode()
 }
 
+type Boolean struct {
+	Token token.Token // the token.BOOLEAN token
+	Value bool
+}
+
+func (b *Boolean) expressionNode() {
+
+}
+
+func (b *Boolean) TokenLiteral() string {
+	return b.Token.Literal
+}
+
+func (b *Boolean) String() string {
+	return b.Token.Literal
+}
+
+type InfixExpression struct {
+	Token    token.Token // the operator token, e.g., '+'
+	Left     Expression
+	Operator string
+	Right    Expression
+}
+
+func (ie *InfixExpression) expressionNode() {
+
+}
+
+func (ie *InfixExpression) TokenLiteral() string {
+	return ie.Token.Literal
+}
+
+func (ie *InfixExpression) String() string {
+	var out bytes.Buffer
+
+	out.WriteString("(")
+	out.WriteString(ie.Left.String())
+	out.WriteString(" " + ie.Operator + " ")
+	out.WriteString(ie.Right.String())
+	out.WriteString(")")
+
+	return out.String()
+}
+
 type PrefixExpression struct {
 	Token    token.Token // the prefix token, e.g., '!'
 	Operator string
